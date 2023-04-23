01b4aae0cf6971aae79c770030954d1cbe288bca
triabagus <triatop9@gmail.com>
2023-04-23 20:34:31 +0700

    Scroll Reveal enable section

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,5 +113,5 @@ const sr = ScrollReveal({
 sr.reveal(`.home__data, .list__container, .join__content, .footer__container`)
 sr.reveal(`.home__img`, {origin: 'bottom'})
 sr.reveal(`.health__img, .routine__images, .follow__img-3`, {origin: 'left'})
-sr.reveal(`.health__data, .routine__data, .follow__img-4`, { origin: "right" });
-sr.reveal(`.follow__data, .follow__content-1 img`, {interval: 100})
+sr.reveal(`.health__data, .routine__data, .follow__img-4`, {origin: 'right'});
+sr.reveal(`.follow__data, .follow__content-1 img, .health__rate, .health__course`,{interval: 100});
